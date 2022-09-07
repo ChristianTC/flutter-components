38fc912955d6b5443c8be039a26aebd9fdac4b6b
Christian Tambo <christiantambo15@gmail.com>
2022-09-07 16:00:16 -0400

    card widget

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fl_components/themes/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class CardScreen extends StatelessWidget {
@@ -5,10 +6,26 @@ class CardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('CardScreen'),
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Card Widget'),
+        ),
+        body: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          children: [
+            Card(
+              child: Column(
+                children: const [
+                  ListTile(
+                    leading: Icon(Icons.phone, color: AppTheme.primary),
+                    title: Text('I am a title'),
+                    subtitle: Text(
+                        'Cillum non aliquip do amet dolore. Enim pariatur est labore dolore labore anim. Dolor nisi voluptate ad duis velit exercitation reprehenderit sunt officia pariatur. Et ex nulla enim proident irure irure culpa exercitation qui. Proident sunt voluptate nisi officia excepteur ut labore mollit quis dolore.'),
+                  )
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
