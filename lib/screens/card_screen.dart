20c5a8440cd125e491a98def21473f4c4d9a0216
Christian Tambo <christiantambo15@gmail.com>
2022-09-08 09:38:16 -0400

    add arguments to Widgets

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -15,7 +15,27 @@ class CardScreen extends StatelessWidget {
           children: const [
             CustomCardType1(),
             SizedBox(height: 20),
-            CustomCardType2(),
+            CustomCardType2(
+                imageUrl:
+                    'https://www.fayerwayer.com/resizer/5ttEUNkOhckxSflC4GJcO479VXc=/arc-photo-metroworldnews/arc2-prod/public/W6A3BTRSWBEYVDVFD4ZBO57RZE.jpg',
+                description: 'Lorem elit duis occaecat cillum ut.'),
+            SizedBox(height: 20),
+            CustomCardType2(
+              imageUrl: 'https://img2.rtve.es/i/?w=1600&i=1657019155649.jpg',
+              description:
+                  'Fugiat voluptate proident pariatur anim ea voluptate ut est ad nisi.',
+            ),
+            SizedBox(height: 20),
+            CustomCardType2(
+              imageUrl:
+                  'https://as01.epimg.net/meristation/imagenes/2022/08/24/noticias/1661328674_157916_1661328877_noticia_normal.jpg',
+            ),
+            SizedBox(height: 20),
+            CustomCardType2(
+                imageUrl:
+                    'https://cl.buscafs.com/www.levelup.com/public/uploads/images/756787/756787.jpg',
+                description:
+                    'Dolor consectetur qui culpa commodo in cillum ad laboris et.'),
           ],
         ));
   }
--- lib/widgets/custom_card_type_2.dart
+++ lib/widgets/custom_card_type_2.dart
@@ -2,7 +2,11 @@ import 'package:fl_components/themes/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class CustomCardType2 extends StatelessWidget {
-  const CustomCardType2({Key? key}) : super(key: key);
+  final String imageUrl;
+  final String? description;
+
+  const CustomCardType2({Key? key, required this.imageUrl, this.description})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -14,19 +18,19 @@ class CustomCardType2 extends StatelessWidget {
       elevation: 10,
       shadowColor: AppTheme.primary.withOpacity(0.5),
       child: Column(children: [
-        const FadeInImage(
-          image: NetworkImage(
-              'https://cloudfront-us-east-1.images.arcpublishing.com/infobae/SXU7HVDRJNDLJLDC5D755OYVU4.jpg'),
-          placeholder: AssetImage('lib/assets/jar-loading.gif'),
+        FadeInImage(
+          image: NetworkImage(imageUrl),
+          placeholder: const AssetImage('lib/assets/jar-loading.gif'),
           width: double.infinity,
           height: 260,
           fit: BoxFit.cover,
-          fadeInDuration: Duration(milliseconds: 300),
+          fadeInDuration: const Duration(milliseconds: 300),
         ),
         Container(
           alignment: AlignmentDirectional.centerEnd,
-          padding: const EdgeInsets.only(right: 20, top: 10, bottom: 10),
-          child: const Text('A super saiyan'),
+          padding:
+              const EdgeInsets.only(left: 20, right: 20, top: 10, bottom: 10),
+          child: Text(description ?? 'Do not have a title'),
         )
       ]),
     );
