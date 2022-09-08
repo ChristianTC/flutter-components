98a7133dfd20f129a6fac2795640a0d72dabc7df
Christian Tambo <christiantambo15@gmail.com>
2022-09-08 13:14:55 -0400

    show alerts

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -1,13 +1,105 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AlertScreen extends StatelessWidget {
   const AlertScreen({Key? key}) : super(key: key);
 
+  void displayDialogIOS(BuildContext context) {
+    showCupertinoDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (context) {
+        return CupertinoAlertDialog(
+          title: const Text('title'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              Text('This is content'),
+              SizedBox(
+                height: 10,
+              ),
+              FlutterLogo(
+                size: 100,
+              )
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Ok'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void displayDialogAndroid(BuildContext context) {
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          elevation: 5,
+          title: const Text('Title'),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              Text('This is content'),
+              SizedBox(
+                height: 10,
+              ),
+              FlutterLogo(
+                size: 100,
+              )
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Ok'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('AlertScreen'),
+        child: ElevatedButton(
+          onPressed: () => Platform.isAndroid
+              ? displayDialogAndroid(context)
+              : displayDialogIOS(context),
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+              'Show alert',
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.close),
+        onPressed: () {
+          Navigator.pop(context);
+        },
       ),
     );
   }
--- lib/themes/app_theme.dart
+++ lib/themes/app_theme.dart
@@ -12,13 +12,27 @@ class AppTheme {
   );
 
   static final ThemeData darkTheme = ThemeData.dark().copyWith(
-      primaryColor: Colors.indigo,
-      appBarTheme: const AppBarTheme(
-        color: primary,
+    primaryColor: Colors.indigo,
+    appBarTheme: const AppBarTheme(
+      color: primary,
+      elevation: 0,
+    ),
+    scaffoldBackgroundColor: Colors.black,
+    textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(primary: primary),
+    ),
+
+    // FloatingActionButtons
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: primary,
+      elevation: 5,
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        primary: primary,
         elevation: 0,
+        shape: const StadiumBorder(),
       ),
-      scaffoldBackgroundColor: Colors.black,
-      textButtonTheme: TextButtonThemeData(
-        style: TextButton.styleFrom(primary: primary),
-      ));
+    ),
+  );
 }
