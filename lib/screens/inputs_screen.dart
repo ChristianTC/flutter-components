90707449f388794b88df11dd939ea6af97163ba2
Christian Tambo <christiantambo15@gmail.com>
2022-09-09 11:49:12 -0400

    center the inputs style

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fl_components/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class InputsScreen extends StatelessWidget {
@@ -16,41 +17,10 @@ class InputsScreen extends StatelessWidget {
             vertical: 10,
           ),
           child: Column(
-            children: [
-              TextFormField(
-                autofocus: true,
-                initialValue: '',
-                textCapitalization: TextCapitalization.words,
-                cursorColor: Colors.yellow,
-                onChanged: (value) {
-                  print(value);
-                },
-                validator: (value) {
-                  if (value == null) {
-                    return 'This field is necessary';
-                  }
-                  return value.length < 3 ? 'min 3 characters' : null;
-                },
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                decoration: const InputDecoration(
-                  hintText: 'User Name',
-                  labelText: 'Name',
-                  helperText: 'Only letters',
-                  prefixIcon: Icon(Icons.person_add),
-                  suffixIcon: Icon(Icons.person_outline_outlined),
-                  icon: Icon(Icons.abc_outlined),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.green,
-                    ),
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(10),
-                      topRight: Radius.circular(10),
-                    ),
-                  ),
-                ),
+            children: const [
+              CustomInputField(
+                labelText: 'Name',
+                hintText: 'User Name',
               ),
             ],
           ),
