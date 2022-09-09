9d537faeacc15f343a6995dc3618994f2c272849
Christian Tambo <christiantambo15@gmail.com>
2022-09-09 11:24:32 -0400

    Text form field personalization

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -19,12 +19,38 @@ class InputsScreen extends StatelessWidget {
             children: [
               TextFormField(
                 autofocus: true,
-                initialValue: 'Christian',
+                initialValue: '',
                 textCapitalization: TextCapitalization.words,
                 cursorColor: Colors.yellow,
                 onChanged: (value) {
                   print(value);
                 },
+                validator: (value) {
+                  if (value == null) {
+                    return 'This field is necessary';
+                  }
+                  return value.length < 3 ? 'min 3 characters' : null;
+                },
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                decoration: const InputDecoration(
+                  hintText: 'User Name',
+                  labelText: 'Name',
+                  helperText: 'Only letters',
+                  prefixIcon: Icon(Icons.person_add),
+                  suffixIcon: Icon(Icons.person_outline_outlined),
+                  icon: Icon(Icons.abc_outlined),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.green,
+                    ),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(10),
+                      topRight: Radius.circular(10),
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
